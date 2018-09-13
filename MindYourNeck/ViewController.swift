acb20443c03fee5620e652fb1c5ed8ca9d59e0ce
locvicvn <32094007+locvicvn@users.noreply.github.com>
2018-09-13 22:52:21 +0700

    Initial Commit

---
+++ MindYourNeck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MindYourNeck
+//
+//  Created by Hong Loc on 9/13/18.
+//  Copyright © 2018 Hong Loc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
